03c455d077b40bb93c2fa74c94b4c061216162b2
Lorena Pojar <lorena.pojar@msg.group>
2022-02-07 15:17:28 +0200

    send + receive example

--- src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
+++ src/main/java/guru/springframework/sfgjms/listener/HelloMessageListener.java
@@ -2,16 +2,23 @@ package guru.springframework.sfgjms.listener;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
 import javax.jms.Message;
+import java.util.UUID;
 
+@RequiredArgsConstructor
 @Component
 public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
 
     //@Payload annotation tells the jms to deserialize that message
     @JmsListener(destination = JmsConfig.MY_QUEUE)
@@ -25,5 +32,18 @@ public class HelloMessageListener {
         //if the client does not confirm that he has received the message, in this case because it throws an exception
         //the message will be queued again for redelivery, and redelivered, until the client says he received the message
         //throw new RuntimeException("whatever");
+    }
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage,
+                               @Headers MessageHeaders headers,
+                               Message message) throws JMSException {
+
+        HelloWorldMessage payloadMsg = HelloWorldMessage.builder()
+                .id(UUID.randomUUID())
+                .message("World!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), payloadMsg);
     }
 }
